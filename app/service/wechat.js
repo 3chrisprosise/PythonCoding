35037675cd19f22e18a8bbc3e1043a6186a81ca6
3chrisprosise <1337074512@qq.com>
2018-03-06 10:45:19 +0800

    bug

--- app/service/wechat.js
+++ app/service/wechat.js
@@ -22,7 +22,7 @@ class WechatService extends Service {
     let array = new Array(token, timestamp, nonce);
     let str = array.sort().join('');
     let sha1Code = Crypto.createHash("sha1");
-    let code = sha1Code.update(str, 'utf-8').digist("hex"); 
+    let code = sha1Code.update(str, 'utf-8').digest("hex"); 
     console.log('varidate success!!!!!!!!!!!!!!!!')
     return code === signature ? echostr : "err"
   }
