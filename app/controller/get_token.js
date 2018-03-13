1544c9fb01407b6c0f72ff0b0679b32a00d002ea
3chrisprosise <1337074512@qq.com>
2018-03-13 16:26:06 +0800

    测试token

---
+++ app/controller/get_token.js
@@ -0,0 +1,19 @@
+'use strict'
+
+const Controller = require('egg').Controller
+const Axios = require('axios')
+
+class GetTokenController extends Controller {
+
+  constructor(ctx){
+    super(ctx)
+  }
+
+  async getToken(){
+    let appid = this.config.wechat_config.appid;
+    let secret = this.config.wechat_config.appSecret;
+    let grant_type = 'client_credential'
+    let token = Axios.get(`https://api.weixin.qq.com/cgi-bin/token?grant_type=${grant_type}&appid=${appid}&secret=${secret}`)
+  }
+
+}
